b232f7a3a738641763599e969ec2e279cec76053
subhanshi <subhanshirawat@gmail.com>
2020-11-16 11:21:24 +0300

    first commit

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def gmap(request):
+    return render(request,'gmap.html')
